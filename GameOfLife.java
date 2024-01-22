ed785a1f2b3c38bab293b0e6f0e3fc94849a1dd8
MayaBenHarush <maya.benharush@post.runi.ac.il>
2024-01-22 10:03:59 +0200

    maya hw 05

--- GameOfLife.java
+++ GameOfLife.java
@@ -11,7 +11,7 @@ public class GameOfLife {
 		String fileName = args[0];
 		//// Uncomment the test that you want to execute, and re-compile.
 		//// (Run one test at a time).
-		//// test1(fileName);
+		// test1(fileName);
 		//// test2(fileName);
 		//// test3(fileName, 3);
 		//// play(fileName);
@@ -63,16 +63,39 @@ public class GameOfLife {
 		int rows = Integer.parseInt(in.readLine());
 		int cols = Integer.parseInt(in.readLine());
 		int[][] board = new int[rows + 2][cols + 2];
-		//// Replace the following statement with your code.
-		return null;
+		int index = 0;
+		while( ! in.isEmpty()){
+			index++;
+			String str= in.readLine(); //the string at each line
+			if(str.length() > 0 ){ //if there is an order at that line
+				for( int j = 0 ; j < str.length() ; j++){
+					if( str.charAt(j) == '.'){
+						board[index][j+1]=0;
+					}
+					if( str.charAt(j) == 'x'){
+						board[index][j+1]=1;
+					}	
+					
+				}
+			}
+			
+		}
+
+
+		return board;
 	}
 	
 	// Creates a new board from the given board, using the rules of the game.
 	// Uses the cellValue(board,i,j) function to compute the value of each 
 	// cell in the new board. Returns the new board.
 	public static int[][] evolve(int[][] board) {
-		//// Replace the following statement with your code.
-		return null;
+		for( int i = 0; i < board.length; i++){
+			for( int j = 0; j < board[0].length; j++){
+				board[i][j] = cellValue(board, i, j);
+			}
+
+		}
+		return board;
 	}
 
 	// Returns the value that cell (i,j) should have in the next generation.
@@ -85,8 +108,25 @@ public class GameOfLife {
 	// Assumes that j is at least 1 and at most the number of columns in the board - 1. 
 	// Uses the count(board,i,j) function to count the number of alive neighbors.
 	public static int cellValue(int[][] board, int i, int j) {
-		//// Replace the following statement with your code.
-		return 0;
+		
+
+		if(board[i][j] == 1 && count( board,i,j) < 2){ //f the cell is alive and has fewer than two live neighbors, it dies.
+			board[i][j] = 0;
+		}
+
+		if( board[i][j] == 1 && (count( board,i,j) == 2 || count( board,i,j) == 3)){  //If the cell is alive and has two or three live neighbors, it remains alive.
+			board[i][j] = 1;
+		}
+		
+		if(board[i][j] == 1 && count( board,i,j) >3 ){ // If the cell is alive and has more than three live neighbors, it dies.
+			board[i][j] = 0;
+		}
+
+		if(board[i][j] == 0 && count( board,i,j) == 3 ){ // If the cell is dead and and has three live neighbors, it becomes alive.
+			board[i][j] = 1;
+		}
+
+		return board[i][j];
 	}
 	
 	// Counts and returns the number of living neighbors of the given cell
@@ -94,13 +134,51 @@ public class GameOfLife {
 	// Assumes that i is at least 1 and at most the number of rows in the board - 1. 
 	// Assumes that j is at least 1 and at most the number of columns in the board - 1. 
 	public static int count(int[][] board, int i, int j) {
-		//// Replace the following statement with your code.
-		return 0;
+		
+		int countAliveN= 0; //count alive neighbors
+		if(board[i][j-1]== 1){
+			countAliveN ++;
+		}
+		
+		if(board[i][j+1]== 1){
+			countAliveN ++;
+		}
+		
+		if(board[i+1][j]== 1){
+			countAliveN ++;
+		}
+		
+		if(board[i-1][j]== 1){
+			countAliveN ++;
+		}
+		
+		if(board[i+1][j+1]== 1){
+			countAliveN ++;
+		}
+		
+		if(board[i+1][j-1]== 1){
+			countAliveN ++;
+		}
+		
+		if(board[i-1][j+1]== 1){
+			countAliveN ++;
+		}
+		
+		if(board[i-1][j-1]== 1){
+			countAliveN ++;
+		}
+		
+		return countAliveN;
 	}
 	
 	// Prints the board. Alive and dead cells are printed as 1 and 0, respectively.
     public static void print(int[][] arr) {
-		//// Write your code here.
+		for (int i = 1; i < arr.length-1; i++) {
+			for (int j = 1; j < arr[0].length-1; j++) {
+			System.out.printf("%3s", arr[i][j]);
+			}
+			System.out.println();
+		}
 	}
 		
     // Displays the board. Living and dead cells are represented by black and white squares, respectively.
